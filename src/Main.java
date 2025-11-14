e64ad7a620d5ae8f97dfdd64677432a69fd87dc3
Kamil Sudol <kamil.sudol5@gmail.com>
2025-11-14 18:34:03 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+
+    public static void main(String [] args) {
+
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+
+        calculateScore(75);
+        calculateScore();
+
+
+    }
+    public static int calculateScore(String playerName, int score){
+
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+
+    }
+    public static int calculateScore( int score){
+
+        return calculateScore("Anonymous", score);
+
+    }
+    public static int calculateScore(){
+
+        System.out.println("Unnamed player no player scored points");
+        return 0;
+
+    }
+
+
+}
