12deaf9c12a961bcfc07cedf93635717e73d07cf
Kamil Sudol <kamil.sudol5@gmail.com>
2025-11-27 19:44:18 +0100

    while loop challenge - adding sum of digits without len etc.

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -1,29 +1,29 @@
 
 public class mainChallenge {
+
     public static void main(String[] args) {
+        System.out.println(sumDigits(1357));
+        System.out.println(sumDigits(9999));
+        System.out.println(sumDigits(0000));
+        System.out.println(sumDigits(1234567));
+        System.out.println(sumDigits(9908700));
 
-        int number = 4;
-        int evenCounter = 0;
-        int oddCounter = 0;
-
-        while (number <= 20){
-            number++;
-
-            if(!isEvenNumber(number)){
-                oddCounter++;
-                System.out.println(number + " is odd number");
-                continue;
-            }
-            System.out.println(number + " is even number");
-            evenCounter++;
-            if(evenCounter == 5){
-                break;
-            }
-        }
-        System.out.println("There are " + evenCounter + " even numbers and " + oddCounter + " odd numbers");
     }
-    public static boolean isEvenNumber(int number){
 
-        return number % 2 == 0;
+    public static int sumDigits(int number) {
+
+        int sum = 0;
+        int divisor = 10;
+
+        if (number < 0) {
+            return -1;
+        }
+
+        while (number > 9) {
+            sum += number % divisor;
+            number /= divisor;
+        }
+        sum += number;
+        return sum;
     }
 }
