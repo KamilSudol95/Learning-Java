a1fcff4bb3a5df3ae87963d3b838130599742668
Kamil Sudol <kamil.sudol5@gmail.com>
2025-11-27 18:39:53 +0100

    another while loop challenge

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -1,18 +1,28 @@
 
 public class mainChallenge {
-
     public static void main(String[] args) {
-        int number = 5;
-        while (number <= 20) {
-            if (isEvenNumber(number)) {
-                System.out.println(number + " is even number");
-            } else {
-                System.out.println(number + " is odd number");
-            }
+
+        int number = 4;
+        int evenCounter = 0;
+        int oddCounter = 0;
+
+        while (number <= 20){
             number++;
+
+            if(!isEvenNumber(number)){
+                oddCounter++;
+                System.out.println(number + " is odd number");
+                continue;
+            }
+            System.out.println(number + " is even number");
+            evenCounter++;
+            if(evenCounter == 5){
+                break;
+            }
         }
+        System.out.println("There are " + evenCounter + " even numbers and " + oddCounter + " odd numbers");
     }
-    public static boolean isEvenNumber(int number) {
+    public static boolean isEvenNumber(int number){
 
         return number % 2 == 0;
     }
