2f55c5732d42862306131e1ce05c4bce1bee51dd
Kamil Sudol <kamil.sudol5@gmail.com>
2025-12-01 19:12:27 +0100

    3rd while loop excercise challenge for 1st of December

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -2,28 +2,28 @@
 public class mainChallenge {
 
     public static void main(String[] args) {
-        System.out.println(sumFirstAndLastDigit(252));
-        System.out.println(sumFirstAndLastDigit(257));
-        System.out.println(sumFirstAndLastDigit(0));
-        System.out.println(sumFirstAndLastDigit(5));
-        System.out.println(sumFirstAndLastDigit(-10));
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+
+
     }
 
-    public static int sumFirstAndLastDigit(int number) {
+    public static int getEvenDigitSum(int number) {
+
+        int sum = 0;
 
         if (number < 0) {
             return -1;
         }
 
-        int firstDigit = number % 10;
-        int lastDigit = number;
-
-        while (number > 9){
-
+        while (number > 0) {
+            int digit = number % 10;
+            if(digit % 2 == 0){
+                sum += digit;
+            }
             number /= 10;
-            lastDigit = number % 10;
-
         }
-        return firstDigit + lastDigit;
+        return sum;
     }
 }
