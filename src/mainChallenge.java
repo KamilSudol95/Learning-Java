5fde61ab79bd94385e0e2bc7ed29bd45ce03095f
Kamil Sudol <kamil.sudol5@gmail.com>
2025-12-01 19:52:56 +0100

    7th while loop excercise challenge for 1st of December

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -3,20 +3,27 @@ public class mainChallenge {
 
     public static void main(String[] args) {
 
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+
     }
 
-    public static void printFactors(int number){
+    public static int getGreatestCommonDivisor(int first, int second){
 
-        if (number < 1){
-            System.out.println("Invalid Value");
+        if (first < 10 || second < 10) {
+            return -1;
         }
 
-        for (int i = 1; i <= number; i++){
-            if (number % i == 0){
-                System.out.println(i);
-                System.out.println(i);
+        int biggestDivider = 0;
+
+        for (int i = 1; i <= first && i <= second; i++) {
+            if (first % i == 0 && second % i == 0) {
+                biggestDivider = i;
             }
-        }
 
+        }
+        return biggestDivider;
     }
 }
