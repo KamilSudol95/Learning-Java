7bc415284beb1a9cadd8be1618fd1633f9f318a4
Kamil Sudol <kamil.sudol5@gmail.com>
2025-12-01 19:47:09 +0100

    6th while loop excercise challenge for 1st of December

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -3,30 +3,20 @@ public class mainChallenge {
 
     public static void main(String[] args) {
 
-        System.out.println(hasSameLastDigit(41, 22, 71));
-        System.out.println(hasSameLastDigit(23, 32, 42));
-        System.out.println(hasSameLastDigit(9, 99, 999));
-
     }
 
-    public static boolean hasSameLastDigit(int num1, int num2, int num3){
+    public static void printFactors(int number){
 
-        int lastDigit1 = num1 % 10;
-        int lastDigit2 = num2 % 10;
-        int lastDigit3 = num3 % 10;
-
-        if(num1 < 10 || num2 < 10 || num3 < 10 || num1 > 1000 || num2 > 1000 || num3 > 1000){
-            return false;
+        if (number < 1){
+            System.out.println("Invalid Value");
         }
 
-        return lastDigit1 == lastDigit2 || lastDigit2 == lastDigit3 || lastDigit1 == lastDigit3;
-    }
-
-    public static boolean isValid(int number){
-
-        if (number < 10 || number > 1000){
-            return false;
+        for (int i = 1; i <= number; i++){
+            if (number % i == 0){
+                System.out.println(i);
+                System.out.println(i);
+            }
         }
-        return true;
+
     }
 }
