a361ec6b965337d090222f6971e2cc2171f87d6b
Kamil Sudol <kamil.sudol5@gmail.com>
2025-12-02 18:32:47 +0100

    2nd challenge for Java Masterclass on 2nd of December

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -2,104 +2,31 @@
 public class mainChallenge {
 
     public static void main(String[] args) {
-
-        System.out.println(getDigitCount(0));
-        System.out.println(getDigitCount(123));
-        System.out.println(getDigitCount(-12));
-        System.out.println(getDigitCount(5200));
-
-        System.out.println(reverse(-121));
-        System.out.println(reverse(1212));
-        System.out.println(reverse(1234));
-        System.out.println(reverse(100));
-
-        //numberToWords(123);
-        //numberToWords(1010);
-        //numberToWords(1000);
-        //numberToWords(-12);
-        //numberToWords(0);
-        //numberToWords(1450);
-
-
-
+        System.out.println(canPack(1, 0, 4));
+        System.out.println(canPack(1, 0, 5));
+        System.out.println(canPack(0, 5, 4));
+        System.out.println(canPack(2, 2, 11));
+        System.out.println(canPack(-3, 2, 12));
+        System.out.println(canPack(5, 3, 24));
     }
 
-    public static void numberToWords (int number){
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
 
+        int maxBigBag = goal / 5;
+        int bigUsed;
+        int remainder;
 
-        int reversed = reverse(number);
-        int originalDigitCount = getDigitCount(number);
-        int reversedDigitCount = getDigitCount(reversed);
-        int missingZeroes = originalDigitCount - reversedDigitCount;
+        if(bigCount < 0 || smallCount < 0 || goal < 0) return false;
 
-        if (number < 0){
-            System.out.println("Invalid Value");
+        if (bigCount > maxBigBag){
+            bigUsed = maxBigBag;
+        }
+        else {
+            bigUsed = bigCount;
         }
 
-        if (number == 0) {
-            System.out.println("Zero");
-            return;
-        }
+        remainder = goal - bigUsed * 5;
 
-        while (reversed > 0){
-            int digit = reversed % 10;
-
-            switch (digit){
-                case 0 -> System.out.println("Zero");
-                case 1 -> System.out.println("One");
-                case 2 -> System.out.println("Two");
-                case 3 -> System.out.println("Three");
-                case 4 -> System.out.println("Four");
-                case 5 -> System.out.println("Five");
-                case 6 -> System.out.println("Six");
-                case 7 -> System.out.println("Seven");
-                case 8 -> System.out.println("Eight");
-                case 9 -> System.out.println("Nine");
-            }
-
-            reversed /= 10;
-
-        }
-        for (int i = 0; i < missingZeroes; i++){
-            System.out.println("Zero");
-        }
-
-    }
-
-    public static int reverse (int number){
-
-        boolean isNegative = false;
-        int reversedNumber = 0;
-
-        if (number < 0) {
-            isNegative = true;
-            number *= -1;
-        }
-
-        while (number > 0){
-            reversedNumber = reversedNumber * 10 + number % 10;
-            number /= 10;
-        }
-        if (isNegative) reversedNumber *= -1;
-
-        return reversedNumber;
-    }
-
-    public static int getDigitCount (int number){
-
-        int counter = 0;
-
-        if (number < 0){
-            return -1;
-        }
-        else if (number == 0){
-            return 1;
-        }
-
-        while (number > 0){
-            counter++;
-            number /= 10;
-        }
-        return counter;
+        return smallCount >= remainder;
     }
 }
