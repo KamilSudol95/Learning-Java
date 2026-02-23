7447e70302eab5b19346876528666ac5b704b3ea
Kamil Sudol <kamil.sudol5@gmail.com>
2026-02-23 23:29:30 +0100

    Java - constructors challenge

--- src/mainChallenge.java
+++ src/mainChallenge.java
@@ -3,35 +3,22 @@ public class mainChallenge {
 
     public static void main(String[] args) {
 
-        BankAccount account = new BankAccount();
-        BankAccount testAccount = new BankAccount(
-                98765432,
-                1500,
-                "Phill Collins",
-                "phill.collins@gmail.com",
-                "44500501502"
-        );
-        BankAccount testAccount2 = new BankAccount();
 
-        account.setAccountNumber( 123456789);
-        account.setBalance(1000.00);
-        account.setCustomerName("Kamil Sudol");
-        account.setEmail("test@gmail.com");
-        account.setPhoneNumber("112554665");
-
-        System.out.println(account.getAccountNumber());
-        System.out.println(account.getBalance());
-        System.out.println(account.getCustomerName ());
-        System.out.println(account.getEmail());
-        System.out.println(account.getPhoneNumber());
+        Customer customer = new Customer("Kamil", 10000, "kamil.sudol@gmail.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
 
 
-        account.deposit(500.00);
-        account.withdraw(250.00);
-        account.withdraw(1500.00);
+        Customer customer2 = new Customer();
+        System.out.println(customer2.getName());
+        System.out.println(customer2.getCreditLimit());
+        System.out.println(customer2.getEmail());
 
-        System.out.println(testAccount);
-        System.out.println(testAccount2.getEmail());
+        Customer customer3 = new Customer("Michael", "michael.angelo@gmail.com");
+        System.out.println(customer3.getName());
+        System.out.println(customer3.getCreditLimit());
+        System.out.println(customer3.getEmail());
 
     }
 }
