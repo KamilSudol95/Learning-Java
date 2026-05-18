563d123a89199a1612d3267b849f493be7b53b63
Kamil Sudol <kamil.sudol5@gmail.com>
2026-05-18 18:03:02 +0200

    Text Block implementation

--- Learning Java/Inheritance/src/anotherMain.java
+++ Learning Java/Inheritance/src/anotherMain.java
@@ -2,8 +2,15 @@ public class anotherMain {
 
     public static void main(String[] args) {
 
-        String bulletIt  = "Print a bullted List:\n" + "\u2022 First Point\n" + "\u2022 Second Point\n";
+        String bulletIt  = "Print a bullted List:\n" + "\t\u2022 First Point\n" + "\t\u2022 Second Point\n";
 
         System.out.println(bulletIt);
+
+        String textBlock = """
+                Print a Bulleted List:
+                    \u2022 First Point
+                    \u2022 Second Point""";
+
+        System.out.println(textBlock);
     }
 }
