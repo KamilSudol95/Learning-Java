4bbcf976f8d922bd397f1608a74df0be61c05eb8
Kamil Sudol <kamil.sudol5@gmail.com>
2026-05-18 18:01:04 +0200

    Advanced output formatting

---
+++ Learning Java/Inheritance/src/anotherMain.java
@@ -0,0 +1,9 @@
+public class anotherMain {
+
+    public static void main(String[] args) {
+
+        String bulletIt  = "Print a bullted List:\n" + "\u2022 First Point\n" + "\u2022 Second Point\n";
+
+        System.out.println(bulletIt);
+    }
+}
